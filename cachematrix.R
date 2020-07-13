36c53a8490e15bbf3c0fa1f9c4149a7ca3594f3c
Álvaro López <58607864+alvarod91100@users.noreply.github.com>
2020-07-13 18:03:39 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## These are two functions to cache a matrix inverse
+## To create matrix that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function( matrix ) {
